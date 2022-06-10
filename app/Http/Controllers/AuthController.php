8043805e4a7b72b3d5482cd0f55c8cbd9d38c9c9
Yudi Setiadi Permana <yudi.setiadi.permana@gmail.com>
2022-06-10 21:57:56 +0700

    feat (auth) : create new endpoint of auth verify and add update migration for auth token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -75,4 +75,76 @@ class AuthController extends Controller
 
     }
 
+    /**
+     * Validate access user using nik and password.
+     *
+     * @param 
+     * - string  $nik       : Must be 16 digit 
+     * - string  $password  : Password has been given in register process
+     *
+     * @return object json 
+     */
+    public function verify(Request $request){
+
+        // Get all request params
+        $params = $request->all();
+
+        // Validate params
+        if (empty($params['nik']) || empty($params['password'])) {
+            return ResponseHelper::setErrorResponse('Invalid parameter');
+        }
+
+        // Validate NIK
+        if (strlen($params['nik']) != 16) {
+            return ResponseHelper::setErrorResponse('NIK must be 16 digit');
+        }
+
+        $result = array();
+
+        // Validate NIK in database
+        $getAuth = Auth::where('auth_nik', $params['nik'])->first();
+
+        if (!$getAuth) {
+            return ResponseHelper::setErrorResponse('Invalid NIK');
+        }
+
+        // Validate password
+        if (!Hash::check($params['password'], $getAuth->auth_password)) {
+            return ResponseHelper::setErrorResponse('Invalid password');
+        }
+
+        // Payload token
+        $payload = array(
+                        'id'   => $getAuth->auth_id,
+                        'nik'  => $getAuth->auth_nik,
+                        'role' => $getAuth->auth_role
+                    );
+
+        // Generate token
+        $generateToken = JWT::encode($payload, env('JWT_KEY'), 'HS256');
+
+        try {
+            
+            // Update token into database
+            $value = array(
+                        'auth_token'    => $generateToken,
+                    );
+
+            $getAuth->update($value);
+
+        } catch (Exception $e) {
+             return ResponseHelper::setErrorResponse($e->getMessage());
+        }
+
+        $result = array(
+                    'id'        => $getAuth->auth_id,
+                    'nik'       => $getAuth->auth_nik,
+                    'role'      => $getAuth->auth_role,
+                    'token'     => $generateToken,
+                );
+
+        return ResponseHelper::setResponse('Authentication valid', $result);
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 $router->get('/', function () use ($router) { return 'API'; });
 
 $router->post('auth/register',      'AuthController@register');
+$router->post('auth/verify',      	'AuthController@verify');
 
 
 
